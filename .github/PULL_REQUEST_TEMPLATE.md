dd2fe9c3673e3711d7779c9daabfa0eeacc5ce59
Brian White <bdidthis@Brians-MacBook-Air.local>
2025-10-07 15:34:38 -0400

    Setup CI & templates

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,9 @@
+## Summary
+Short description.
+
+## Tests
+- [ ] Ran on iPhone/Android
+- [ ] Compared BPM/Key vs ground truth
+
+## Notes
+Focus areas for reviewers.
---
+++ test/bpm_estimator_test.dart
@@ -0,0 +1,3 @@
+import 'package:test/test.dart';
+import 'package:harmoniq/bpm_estimator.dart';
+void main() => test('init', () => expect(BpmEstimator().bpm, anyOf(isNull, greaterThan(0))));
